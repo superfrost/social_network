4ea9de8e847488cb12bff75dab673f5a196fa56d
superfrost <otkycu@gmail.com>
2020-04-10 11:05:19 +0300

    fix some bugs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styless from './Dialogs.module.css'
 import { NavLink } from 'react-router-dom'
 
-const Dialog_to_person = (props) => {
+const DialogToPerson = (props) => {
     return (
         <div className={styless.dialog_to_person}>
             <NavLink to={`/dialogs/${props.person_id}`} className={styless.person_link}
@@ -11,7 +11,7 @@ const Dialog_to_person = (props) => {
     )
 }
 
-const One_message = (props) => {
+const OneMessage = (props) => {
     return (
         <div className={styless.one_message}>
             {props.one_message_text}
@@ -19,20 +19,34 @@ const One_message = (props) => {
     )
 }
 
+let dialogsData = [
+    {id: 1, name: 'Nik'},
+    {id: 2, name: 'Mary'},
+    {id: 3, name: 'Den'},
+    {id: 4, name: 'Sara'}
+]
+
+let messageData = [
+    {id: 1, message: "Hi. Realy cool weather! Do you wanna go to park?"},
+    {id: 2, message: "Hi it's true. Meat me near my house in 15."},
+    {id: 3, message: "Ok I'll be there!"},
+    {id: 4, message: "Ok. See you 😊"}
+]
+
 const Dialogs = (props) => {
     return (
         <div className={styless.dialog_container}>
             <div className={styless.dialog_item}>
-                <Dialog_to_person person_name="Nik" person_id="1"/>
-                <Dialog_to_person person_name="Mary" person_id="2"/>
-                <Dialog_to_person person_name="Den" person_id="3"/>
-                <Dialog_to_person person_name="Sara" person_id="4"/>
+                <DialogToPerson person_name={dialogsData[0].name} person_id={dialogsData[0].id}/>
+                <DialogToPerson person_name={dialogsData[1].name} person_id={dialogsData[1].id}/>
+                <DialogToPerson person_name={dialogsData[2].name} person_id={dialogsData[2].id}/>
+                <DialogToPerson person_name={dialogsData[3].name} person_id={dialogsData[3].id}/>
             </div>
             <div className={styless.messages}>
-                <One_message one_message_text="Hi. Realy cool weather! Do you wanna go to park?"/>
-                <One_message one_message_text="Hi it's true. Meat me near my house in 15."/>
-                <One_message one_message_text="Ok I'll be there!"/>
-                <One_message one_message_text="Ok. See you 😊"/>
+                <OneMessage one_message_text={messageData[0].message}/>
+                <OneMessage one_message_text={messageData[1].message}/>
+                <OneMessage one_message_text={messageData[2].message}/>
+                <OneMessage one_message_text={messageData[3].message}/>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import styless from './MyPosts.module.css'
 import Post from './Post/Post'
 
+let postsData = [
+  {id: 1, message: "Hi it's me !!!", like_count: 17},
+  {id: 2, message: "Where are you?", like_count: 4}
+]
+
 const MyPosts = (props) => {
   return (
     <div>
@@ -9,8 +14,8 @@ const MyPosts = (props) => {
         All posts
       </div>
       <div className={styless.posts}>
-        <Post message="Hi it's me !!!" like_count="17"></Post>
-        <Post message="Where are you?" like_count="4"></Post>
+        <Post message={postsData[0].message} like_count={postsData[0].like_count}></Post>
+        <Post message={postsData[1].message} like_count={postsData[1].like_count}></Post>
       </div>
     </div>
   )
